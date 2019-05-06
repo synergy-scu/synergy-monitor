b82231c47f7ede2907a62a2a762ef73c3b6f869f
Pearce <pearce.ropion@me.com>
2019-05-05 20:19:46 -0700

    monitor changes

--- synergy/publisher.py
+++ synergy/publisher.py
@@ -1,8 +1,9 @@
 import paho.mqtt.publish as publish
  
-MQTT_SERVER = '192.168.0.39'
+MQTT_SERVER = '192.168.0.31'
 MQTT_PATH = 'hub_channel'
 
 def publish_data(data):
+	# print(data)
 	publish.single(MQTT_PATH, data, hostname=MQTT_SERVER)
 	return
